ef19ab32b118443cb7a4db11d5f8de1510cd8b71
Oleg Sheinin <oleg.sheinin@oracle.com>
2018-06-27 21:52:35 +0300

    implemented old new URL handling. Still having problem with SSL. Will seek for solution

--- windows/BBDailyLessonsDownloader/BBDailyLessonsDownloader/FDConstants.cs
+++ windows/BBDailyLessonsDownloader/BBDailyLessonsDownloader/FDConstants.cs
@@ -10,7 +10,8 @@ namespace BBLessonsDwnldApp
         //public static string httpServerPath = "http://dl.dropbox.com/u/22519537/"; //my dropbox
         //public static string ftpServerPath = "http://dl.dropbox.com/u/3074981/";
         //public static string ftpServerPath = "http://upload.kbb1.com/lessondn/";
-        public static string mylibraryLessonList = "http://old.kabbalahmedia.info/api/morning_lessons.json";
+        // https://old.kabbalahmedia.info/morning_lesson/ENG
+        public static string mylibraryLessonList = "http://old.kabbalahmedia.info/morning_lesson";
         public static string txtFileToday;
         public static string dateFormat = "yyyy-MM-dd";
         public static string[] languagesList = new string[] { "Hebrew", "English", "Russian", "Spanish", "German", "Italian", "French", "Turkish", "Swedish", "Portuguese" };
